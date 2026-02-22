c86591dac852bbcc4d927d8cc53f05ee7b7373aa
Sumit Gouthaman <sumitgt007@gmail.com>
2026-02-21 18:58:13 -0800

    feat: Configure app as a menu bar extra with dynamic Dock icon visibility based on settings window state.

--- oba-macos/oba_macosApp.swift
+++ oba-macos/oba_macosApp.swift
@@ -6,18 +6,28 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct oba_macosApp: App {
     @StateObject private var store = Store()
-    
+
     var body: some Scene {
         Window("Settings", id: "settings") {
             SettingsView()
                 .environmentObject(store)
+                .onAppear {
+                    // Show in Dock while Settings window is open
+                    NSApp.setActivationPolicy(.regular)
+                    NSApp.activate(ignoringOtherApps: true)
+                }
+                .onDisappear {
+                    // Hide from Dock when Settings window is closed
+                    NSApp.setActivationPolicy(.accessory)
+                }
         }
         .defaultSize(width: 500, height: 600)
-        
+
         MenuBarExtra("OneBusAway", systemImage: "bus") {
             PopoverView()
                 .environmentObject(store)
